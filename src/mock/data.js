c9bfe1f8d60a960d8d50f0bb10692e060ddffc58
Oscar Ravelo <oscar91ravelo@gmail.com>
2021-05-16 16:45:58 +0200

    first launch

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,48 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'portfolio', // e.g: 'Name | Developer'
+  lang: 'es', // e.g: en, es, fr, jp
+  description: 'Welcome to my  PortFolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Oscar Ravelo',
+  subtitle: "I'm a Full Stack Web Dev",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'Oscar Ravelo, Web Developer. 29 years old located in Spain',
+  paragraphTwo:
+    'Im Javascript addict, self taught programmer and Full Stack Web Developer. I have been programming for three years now, which from two years Ive been  learning about web developing.',
+  paragraphThree: 'Open to work in challeging areas',
+
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project1.jpg',
+    title: 'TODO LIST',
+    info: 'A todo list for productivity',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://todo-cat.herokuapp.com/',
+    repo: 'https://github.com/OscarRavelo/TodoAppBlitz', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: 'Send me an Email',
+  email: 'oscar91ravelo@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,22 +52,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      url: 'https://twitter.com/Punkk1x',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/oscar-enrique-ravelo-noguera-310684156/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/OscarRavelo',
     },
   ],
 };
